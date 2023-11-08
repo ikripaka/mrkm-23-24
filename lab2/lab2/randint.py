eb69278fc97434e7b559d9309916246b6ecafe6b
Oleksii Chalyi <33095491+oleksiichalyi@users.noreply.github.com>
2023-11-08 14:47:29 +0200

    Add files via upload

---
+++ lab2/lab2/randint.py
@@ -0,0 +1,7 @@
+from Crypto.Random.random import randint
+
+a = 5  
+b = 15  
+random_integer = randint(a, b)
+
+print(f'random from {a} to {b}: {random_integer}')
---
+++ lab2/lab2/randrange.py
@@ -0,0 +1,9 @@
+from Crypto.Random.random import randrange
+
+result1 = randrange(10) 
+result2 = randrange(5, 15) 
+result3 = randrange(0, 100, 10) 
+
+print(result1)
+print(result2)
+print(result3)
