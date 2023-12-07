bc967c6bd83a31c85523e682d229c3c1926b107d
Dmytro Melnyk <dmytro.melnyk.it@gmail.com>
2023-12-07 11:06:22 +0200

    fix signature

--- lab3/MelnykDmytro_SkudanDanyl_fb21nm/fast-api/main.py
+++ lab3/MelnykDmytro_SkudanDanyl_fb21nm/fast-api/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 @app.get("/generate-signature")
 def generate_signature():
     try:
-        subprocess.run(["docker", "run", "--rm", "-v", "$(pwd)/data:/data", "ecdsa-container", "generate"])
+        subprocess.run(["/fast-api/eds.sh", "generate"])
         return {"message": "Signature generated successfully"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error generating signature: {str(e)}")
@@ -16,7 +16,7 @@ def generate_signature():
 @app.get("/verify-signature")
 def verify_signature():
     try:
-        result = subprocess.run(["docker", "run", "--rm", "-v", "$(pwd)/data:/data", "ecdsa-container", "verify"], capture_output=True)
+        result = subprocess.run(["/fast-api/eds.sh", "verify"], capture_output=True)
         output = result.stdout.decode().strip()
         if "Signature is valid" in output:
             return {"message": "Signature is valid"}
